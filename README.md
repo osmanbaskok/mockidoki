bd4981018a4ccd8062a444fe8c6ea64a2172e6f4
Osman Başkök <osman.baskok@trendyol.com>
2022-08-23 00:29:54 +0300

    Setup Kafka container in docker compose file & revise readme

--- README.md
+++ README.md
@@ -45,6 +45,23 @@ curl -v --location --request POST 'http://localhost:8080/http-mocks/warehouses/3
 
 PS: There are three pre-defined sample HTTP mock records in the http_mock table. You can check and play with them as you wish.
 
+#### Event mock example:
+* Run the following Curl script
+````  
+curl --location --request POST 'http://localhost:8080/event-mocks/stock-created/process' \
+--data-raw '{
+	"productId": 12345,
+	"price": 49.90,
+	"name": "Mockidoki Tshirt"
+}'
+````  
+* Run the following command in the terminal to receive the message sent by the curl script given above (make sure that you first have kafka & zookeper installed on your machine to run the following command)
+````  
+kafka-console-consumer --bootstrap-server localhost:29092 --topic my-mockidoki-topic --from-beginning
+````  
+
+PS: You can also post an array of messages at once to the given API endpoint (http://localhost:8080/event-mocks/{key}}/process-list). They will still be published as a single message on the specified topic.
+
 ### Dependencies
 
 * Postgres DB
--- init.sql
+++ init.sql
@@ -28,3 +28,23 @@ create unique index http_mock_method_murl_mheader_mbody_unique_index
 INSERT INTO public.http_mock (method, matching_url, matching_header, matching_body, response_status, response_body, response_header, description, is_deleted) VALUES ('POST', '(.*)/warehouses/3/stock-transactions(.*)', '(.*)mockidoki-user(.*)', '(.*)(?=.*12345)(?=.*45.37)(.*)', 201, null, '[{"value": "test_perfect1", "header": "test_header1"}, {"value": "test_perfect2", "header": "test_header2"}]', 'Mock for adding new stock transaction', false);
 INSERT INTO public.http_mock (method, matching_url, matching_header, matching_body, response_status, response_body, response_header, description, is_deleted) VALUES ('GET', '(.*)/warehouses/3/stock-transactions\?type=SALES(.*)', null, null, 200, '{"transactions":[{"Id":"6259166211f5d6001565a5dc","ettn":"462B3870-8F69-4FC6-B5E3-7D1E6D47BF25","iNumber":"4272019563da4174b25e4ccc059a75ec","status":"OK","createdDate":"2022-08-15T09:53:22","amount":377.97},{"Id":"6239566211f5d6892465a5df","ettn":"566B3870-8F69-4AC6-B5E3-7B1E6D47BF68","iNumber":"5679019563da4174b25e4ccc059a75fd","status":"OK","createdDate":"2022-08-16T10:57:26","amount":67.15},{"Id":"1439566211f5d6892465a5ab","ettn":"986B3870-8F69-4AF6-A5D3-7B1E6D47BF11","iNumber":"1879019563da4074c25e4ccc059a75ed","status":"OK","createdDate":"2022-08-17T12:21:12","amount":90.25}]}', null, 'Mock stock transaction list', false);
 INSERT INTO public.http_mock (method, matching_url, matching_header, matching_body, response_status, response_body, response_header, description, is_deleted) VALUES ('POST', '(.*)/product(.*)', null, '(.*)991188|17789|828929(.*)', 201, null, null, 'Mock for adding new product (if productId is 991188 or 17789 or 828929)', false);
+
+create table event_mock
+(
+    id          bigserial    not null
+        constraint event_mock_pkey
+            primary key,
+    key         varchar(255) not null,
+    channel     varchar(255) not null,
+    description varchar(255) not null,
+    is_deleted  boolean
+);
+
+alter table event_mock
+    owner to postgres;
+
+create unique index event_mock_unique_key
+    on event_mock (key);
+
+INSERT INTO public.event_mock (key, channel, description, is_deleted) VALUES ('stock-created', 'my-mockidoki-topic', 'when stock created', false);
+
